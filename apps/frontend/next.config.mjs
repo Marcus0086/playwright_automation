e9bf85c838c6a0ce4f3f5340439fe1c0d072ab1a
Marcus0086 <guptamarcus42@gmail.com>
2024-10-11 19:25:04 +0530

    Refactor Dockerfile and next.config.mjs, and update input.tsx

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     env: {
         VNC_URL: process.env.VNC_URL,
         BACKEND_URL: process.env.BACKEND_URL,
